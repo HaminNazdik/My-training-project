453756262ae53ea50de028893ca33d4e6df27a10
kheiliNazdik <pariya1404m@gmail.com>
2026-06-09 12:31:40 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+
 function searchProduct() {
     window.location.href = "search-results.html";
+}
+
+function changeLanguage() {
+    let language = document.getElementById("language").value;
+
+    alert("Selected language: " + language);
 }
